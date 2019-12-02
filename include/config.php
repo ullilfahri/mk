7614e0ffa41175c58fffc9f086730882a5810bf4
ullilfahri <37692168+ullilfahri@users.noreply.github.com>
2019-12-02 04:21:54 -0800

    Initial commit

---
+++ include/config.php
@@ -0,0 +1,9 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['sada'] = array ('1'=>'sada!192.168.1.21','sada@|@admin','sada#|#qJKWmZ+ZaWM=','sada%ww','sada^distrik51.id','sada&Rp','sada*10','sada(1','sada)','sada=10','sada@!@disable');
+$data['ds'] = array ('1'=>'ds!192.168.1.21','ds@|@admin','ds#|#qJKWmZ+ZaWM=','ds%das','ds^distrik51.id','ds&Rp','ds*10','ds(1','ds)','ds=10','ds@!@disable');
+$data['new-1846'] = array ('1'=>'new-1846!','new-1846@|@','new-1846#|#','new-1846%','new-1846^','new-1846&Rp','new-1846*10','new-1846(1','new-1846)','new-1846=10','new-1846@!@disable');
+$data['AMKI'] = array ('1'=>'AMKI!10.10.9.254','AMKI@|@admin','AMKI#|#qJKWmZ+ZaWM=','AMKI%amki.net','AMKI^amki.net','AMKI&Rp','AMKI*10','AMKI(1','AMKI)','AMKI=10','AMKI@!@disable');
+$data['distrik'] = array ('1'=>'distrik!192.168.1.17','distrik@|@admin','distrik#|#qJKWmZ+ZaWM=','distrik%distrik','distrik^distrik51.id','distrik&Rp','distrik*10','distrik(1','distrik)','distrik=10','distrik@!@disable');
